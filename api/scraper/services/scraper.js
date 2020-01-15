ae99aeb35699c4e6992f2b71adb377b0479dc22a
KaKa <kaka@kakawebsitedemo.com>
2020-01-15 23:41:54 +0800

    Update API

---
+++ api/scraper/services/scraper.js
@@ -0,0 +1,246 @@
+"use strict";
+const puppeteer = require("puppeteer");
+const JSDOM = require("jsdom").JSDOM;
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/services.html#core-services)
+ * to customize this service
+ */
+
+function serializeTableLine(dom) {
+  const columns = Array.from(dom.querySelectorAll("td"));
+  const images = Array.from(dom.querySelectorAll("img"));
+  return {
+    columns,
+    images
+  };
+}
+
+function indicator(doms, word) {
+  return doms.findIndex(function(dom) {
+    return dom.innerHTML === word;
+  });
+}
+
+function shouldStart(s) {
+  const { columns } = s;
+  return !!columns.find(function(dom) {
+    return dom.innerHTML === "名稱";
+  });
+}
+
+function isCharacter(s) {
+  const { images } = s;
+  return !!images[0];
+}
+
+function line1parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const GetIndicator = indicator(columns, "取得方式");
+
+  // Body
+  c.Star = images[0] && images[0].src;
+  c.fullbody = [images[1] && images[1].src, images[2] && images[2].src];
+  c.CNGet = columns[GetIndicator + 1].innerHTML;
+}
+
+function line2parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const HPIndicator = indicator(columns, "HP");
+  const Names = String(columns[HPIndicator - 1].innerHTML).split("<br>");
+
+  // Body
+  c.ProPic = images[0] && images[0].src;
+  c.JPName = Names[0];
+  c.CNName = Names[1];
+  c.HP = columns[HPIndicator + 1].innerHTML;
+  c.CNDesc = columns[HPIndicator + 2].innerHTML;
+}
+
+function line3parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const ATKIndicator = indicator(columns, "ATK");
+
+  // Body
+  c.ATK = columns[ATKIndicator + 1].innerHTML;
+}
+
+function line4parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const NickIndicator = indicator(columns, "別稱");
+  const RaceIndicator = indicator(columns, "種族");
+  const Race = String(columns[RaceIndicator + 1].innerHTML).split("/");
+
+  // Body
+  c.JPNickname = columns[NickIndicator + 1].innerHTML;
+  c.Race1 = Race.length > 0 ? Race[0] : undefined;
+  c.Race2 = Race.length > 1 ? Race[1] : undefined;
+}
+
+function line5parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const GenderIndicator = indicator(columns, "性別");
+
+  // Body
+  c.Gender = columns[GenderIndicator + 1].innerHTML;
+  c.Weapon = images[0] && images[0].src;
+}
+
+function line6parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const LeaderBuffIndicator = indicator(columns, "隊長特性");
+  const leaderBuffName = columns[LeaderBuffIndicator + 1].innerHTML;
+  const leaderBuffContent = columns[LeaderBuffIndicator + 2].innerHTML;
+
+  // Body
+  c.CNLeaderBuff = `【${leaderBuffName}】${leaderBuffContent.replace(
+    "<br>",
+    ""
+  )}`;
+}
+
+function line7parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const SkillIndicator = indicator(columns, "技能");
+  const skillName = columns[SkillIndicator + 1].innerHTML;
+  const skillContent = columns[SkillIndicator + 2].innerHTML;
+  const skillCost = Array.from(skillName.match(/\d+/g) || []);
+
+  // Body
+  c.CNSkills = `【${skillName.split("<br>")[0]}】${skillContent.replace(
+    "<br>",
+    ""
+  )}`;
+  c.CNSkillName = skillName.split("<br>")[0];
+  c.CNSkillDesc = skillContent.replace("<br>", "");
+  c.SkillCost = skillCost.length > 0 ? skillCost[0] : 0;
+}
+
+function line8parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const A1Indicator = indicator(columns, "能力 1");
+
+  // Body
+  c.CNAbility1 = columns[A1Indicator + 1].innerHTML;
+}
+
+function line9parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const A2Indicator = indicator(columns, "能力 2");
+
+  // Body
+  c.CNAbility2 = columns[A2Indicator + 1].innerHTML;
+}
+
+function line10parser(s, c) {
+  const { columns, images } = s;
+  // Indicator
+  const A3Indicator = indicator(columns, "能力 3");
+
+  // Body
+  c.CNAbility3 = columns[A3Indicator + 1].innerHTML;
+}
+
+module.exports = {
+  fetch: async function() {
+    // Start Browser
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--single-process"]
+    });
+    // Create New Page
+    const page = await browser.newPage();
+    // Go to Search Path
+    await page.goto(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5OvhecdUnTXEeO2fpdERfiZh3PzadSoGcpQ1IEhAPCSfcv2iLk7p0V7MFiZ7AZNnPVRSzUsRI5Wye/pubhtml#"
+    );
+
+    // Retreive Page Content
+    const html = await page.content();
+    const { window } = new JSDOM(html);
+    const document = window.document;
+
+    // Mapper
+    const RarityMapper = {};
+    const WeaponMapper = {};
+
+    const lines = Array.from(
+      document.querySelectorAll(".grid-container tbody tr")
+    );
+
+    const data = lines.reduce(function(arr, dom, index) {
+      const s = serializeTableLine(dom);
+      if (shouldStart(s) && isCharacter(s)) {
+        const character = {};
+        line1parser(serializeTableLine(lines[index]), character);
+        line2parser(serializeTableLine(lines[index + 1]), character);
+        line3parser(serializeTableLine(lines[index + 2]), character);
+        line4parser(serializeTableLine(lines[index + 3]), character);
+        line5parser(serializeTableLine(lines[index + 4]), character);
+        line6parser(serializeTableLine(lines[index + 5]), character);
+        line7parser(serializeTableLine(lines[index + 6]), character);
+        line8parser(serializeTableLine(lines[index + 7]), character);
+        line9parser(serializeTableLine(lines[index + 8]), character);
+        line10parser(serializeTableLine(lines[index + 9]), character);
+
+        // Images
+
+        let middlepath = "";
+        if (character.CNGet.includes("新年")) middlepath = "/new-year-eve/";
+        if (character.CNGet.includes("聖誕")) middlepath = "/christmas/";
+        character.GifURL = encodeURIComponent(
+          `/assets/wf-characters${middlepath}${character.JPName}.gif`
+        );
+        character.SpriteURL = encodeURIComponent(
+          `/assets/wf-characters${middlepath}${character.JPName}.png`
+        );
+
+        if (character.JPName === "ヴァーグナー") {
+          RarityMapper[character.Star] = 5;
+          WeaponMapper[character.Weapon] = "射撃";
+        }
+
+        if (character.JPName === "アルク") {
+          RarityMapper[character.Star] = 4;
+          WeaponMapper[character.Weapon] = "剣士";
+        }
+
+        if (character.JPName === "リリル") {
+          RarityMapper[character.Star] = 3;
+          WeaponMapper[character.Weapon] = "特殊";
+        }
+
+        if (character.JPName === "ゴーレム") {
+          RarityMapper[character.Star] = 2;
+          WeaponMapper[character.Weapon] = "格闘";
+        }
+
+        if (character.JPName === "ファイアスピリ") {
+          RarityMapper[character.Star] = 1;
+          WeaponMapper[character.Weapon] = "補助";
+        }
+
+        arr.push(character);
+      }
+      return arr;
+    }, []);
+
+    return data.map(function(character) {
+      // remove any html tag if found
+      character.CNName = character.CNName.replace(/<.*?>/g, "");
+      character.CNGet = character.CNGet.replace(/<.*?>/g, "");
+      character.Rarity = RarityMapper[character.Star];
+      character.JPWeapon = WeaponMapper[character.Weapon];
+      character.CNWeapon = WeaponMapper[character.Weapon];
+      return character;
+    });
+  }
+};
---
+++ api/wf-character/controllers/wf-character.js
@@ -0,0 +1,83 @@
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  lookup: async ctx => {
+    let entities = [];
+    if (ctx.query.name) {
+      const lookup = await strapi
+        .query("wf-character")
+        .model.find({
+          $or: [
+            {
+              JPName: {
+                $regex: ctx.query.name,
+                $options: "i"
+              }
+            },
+            {
+              CNName: {
+                $regex: ctx.query.name,
+                $options: "i"
+              }
+            },
+            {
+              Nicknames: {
+                $in: [ctx.query.name]
+              }
+            }
+          ]
+        })
+        .exec();
+      console.log(lookup);
+      if (!!lookup) {
+        entities = entities.concat(lookup);
+      }
+    }
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models["wf-character"] })
+    );
+  },
+  scrape: async ctx => {
+    const entities = await strapi.services.scraper.fetch();
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models["wf-character"] })
+    );
+  },
+  initial: async ctx => {
+    const entities = await strapi.services.scraper.fetch();
+    for (let i = entities.length - 1; i >= 0; i--) {
+      try {
+        const character = entities[i];
+        const query = {
+          JPName: character.JPName,
+          CNName: character.CNName,
+          JPNickname: character.JPNickname,
+          Rarity: character.Rarity,
+          CNGet: character.CNGet
+        };
+        // find if wf character is exist
+        const WFCharacter = await strapi
+          .query("wf-character")
+          .model.findOne(query);
+        if (WFCharacter) {
+          strapi.query("wf-character").update(query, character);
+        } else {
+          // try to create wf character
+          strapi.query("wf-character").create(character);
+        }
+      } catch (err) {
+        // do something if error occur
+        console.debug(err);
+      }
+    }
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models["wf-character"] })
+    );
+  }
+};

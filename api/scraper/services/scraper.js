9de6acac272de76cc80dd2e50e601bfca07b1004
KaKa <kaka@kakawebsitedemo.com>
2020-01-18 19:45:11 +0800

    Fix: &amp;

--- api/scraper/services/scraper.js
+++ api/scraper/services/scraper.js
@@ -256,9 +256,18 @@ module.exports = {
       // remove any html tag if found
       character.CNName = character.CNName.replace(/<.*?>/g, "");
       character.CNGet = character.CNGet.replace(/<.*?>/g, "");
-      character.CNAbility1 = character.CNAbility1.replace(/<.*?>/g, "");
-      character.CNAbility2 = character.CNAbility2.replace(/<.*?>/g, "");
-      character.CNAbility3 = character.CNAbility3.replace(/<.*?>/g, "");
+      character.CNAbility1 = character.CNAbility1.replace(/<.*?>/g, "").replace(
+        /&amp;/g,
+        "&"
+      );
+      character.CNAbility2 = character.CNAbility2.replace(/<.*?>/g, "").replace(
+        /&amp;/g,
+        "&"
+      );
+      character.CNAbility3 = character.CNAbility3.replace(/<.*?>/g, "").replace(
+        /&amp;/g,
+        "&"
+      );
 
       character.Rarity = RarityMapper[character.Star];
       character.JPWeapon = WeaponMapper[character.Weapon];
